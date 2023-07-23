b5ca9e9ab9850c3ecd3307a35aec69134f90b5bf
Volodymyr Molchanov <volodymyr_molchanov@epam.com>
2023-07-23 12:42:40 +0200

    feature: enable https for server, create dockerfile

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,6 +13,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { streakCheckJob } from "@cron-jobs/index";
 import "@listeners/userListeners";
+import { isProdEnv } from "@utils/envUtils";
+import https from "https";
+import fs from "fs";
 
 const loggerMiddleware = (req: Request, _res: Response, next: NextFunction) => {
   console.log(
@@ -27,11 +30,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://34.116.149.220",
-      "http://www.waterproof-jule.online",
-    ],
+    origin: ["http://localhost:3000", "https://www.waterproof-jule.online"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Access-Control-Allow-Headers",
@@ -62,8 +61,29 @@ app.use("/tasks", tasksRouter);
 
 streakCheckJob.start();
 
-connect().then(() => {
-  app.listen(config.port, () => {
-    console.log(`App listening at http://localhost:${config.port}`);
+if (isProdEnv()) {
+  console.log("Running in prod env");
+
+  const httpsOptions = {
+    key: fs.readFileSync("../private_key.pem"),
+    cert: fs.readFileSync("../certificate.pem"),
+  };
+
+  const httpsPort = 443;
+
+  const server = https.createServer(httpsOptions, app);
+
+  connect().then(() => {
+    server.listen(httpsPort, () => {
+      console.log(`App listening at https port`);
+    });
   });
-});
+} else {
+  console.log("Running in dev env");
+
+  connect().then(() => {
+    app.listen(config.port, () => {
+      console.log(`App listening at http://localhost:${config.port}`);
+    });
+  });
+}
